200e4672381565c18825c6564865aa1df0de250e
dorrion <dhdms8696@naver.com>
2022-09-15 17:19:15 +0900

    ✨ feat    : 아이콘 버튼
    - 아이콘 삽입
    Issues: #4

--- src/components/aliance/Button.js
+++ src/components/aliance/Button.js
@@ -1,23 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import data from "data/afl_list.json";
 
 const StyledCircle = styled.button`
   width: 110px;
   height: 110px;
-  border: 2px solid lightcoral;
+  border: none;
   border-radius: 50%;
   color: #000000;
   margin-left: 50px;
   cursor: pointer;
   :hover {
-    background: red;
+    background: ${(props) => props.theme.red};
   }
 `;
 
-function Button(props) {
-  const { title, onClick } = props;
+// Icon CSS
+const Icon = styled.img`
+  width: 72px;
+  height: 72px;
+`;
 
-  return <StyledCircle>{title}</StyledCircle>;
+function Button() {
+  return (
+    <>
+      {data.afl.map((item) => (
+        <StyledCircle>
+          <Icon src={item.content}></Icon>
+        </StyledCircle>
+      ))}
+    </>
+  );
 }
 
 export default Button;
