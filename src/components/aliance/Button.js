53f982638649bc6e8687bf868407056b59dad8fc
dorrion <dhdms8696@naver.com>
2022-09-15 17:33:51 +0900

    ✨ feat    : 아이콘 버튼
    - 제목도 불러오기
    
    Issues: #4

--- src/components/aliance/Button.js
+++ src/components/aliance/Button.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 import data from "data/afl_list.json";
 
 const StyledCircle = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   width: 110px;
   height: 110px;
   border: none;
@@ -25,9 +30,13 @@ function Button() {
   return (
     <>
       {data.afl.map((item) => (
-        <StyledCircle>
-          <Icon src={item.content}></Icon>
-        </StyledCircle>
+        <>
+          <StyledCircle>
+            <Icon src={item.content}></Icon>
+            <div>{item.name}</div>
+          </StyledCircle>
+          
+        </>
       ))}
     </>
   );
