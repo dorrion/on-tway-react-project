8cab3827e243a631d3a9fa264ba96333badb3bcd
dorrion <dhdms8696@naver.com>
2022-09-20 21:50:21 +0900

    🎨 style   : 이미지 스타일 변경

--- src/components/aliance/AfflImage.js
+++ src/components/aliance/AfflImage.js
@@ -4,7 +4,8 @@ import gogi from "assets/images/386844ba-b61d-448e-947a-eb7db24562a1.png"
 
 const LargeImage = styled.img`
   width: 380px;
-  height: 313px;
+  height: 318px;
+  margin-right: 30px;
   cursor: pointer;
 `;
 function AfflImage(props) {
